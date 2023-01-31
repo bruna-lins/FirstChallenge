f062bdd0c7491985b6d9380df66852c2e1954b68
= <=>
2023-01-31 13:37:24 -0300

    client entity

--- src/main/java/com/first/entities/Client.java
+++ src/main/java/com/first/entities/Client.java
@@ -18,7 +18,7 @@ public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.UUID)
 	private Long id;
 	private String name;
 	private String cpf;
